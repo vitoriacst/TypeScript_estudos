edcb89be278e3eeecb8264eceadd8d8fd56cf08d
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-26 18:38:22 -0300

    feat: :sparkles: typing a few variables

---
+++ characters/index.ts
@@ -0,0 +1,11 @@
+type Character = any;
+
+type character = {
+  nickname: string;
+  class: string;
+  stats: { agi: number, str: number, int: number, hp: number, mp: number };
+  createdAt: Date;
+};
+
+
+
