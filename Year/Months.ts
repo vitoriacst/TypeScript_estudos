8905fb09de783dd570fe8f262f09c47ec5e4690e
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-26 18:46:56 -0300

    feat: :sparkles: Enum with the months of the year

---
+++ Year/Months.ts
@@ -0,0 +1,20 @@
+
+
+enum Months {
+  JANEIRO = "Janeiro",
+  FEVEREIRO = "Fevereiro",
+  MARCO = "Março",
+  ABRIL = "Abril",
+  MAIO = "Maio",
+  JUNHO = "Junho",
+  JULHO = "Julho",
+  AGOSTO = "Agosto",
+  SETEMBRO = "Setembro",
+  OUTUBRO = "Outubro",
+  NOVEMBRO = "Novembro",
+  DEZEMBRO = "Dezembro",
+}
+
+export default Months
+
+// criando um enum com os meses do ano
