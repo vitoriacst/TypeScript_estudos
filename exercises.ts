b20f5039ce516dc5b7e32b178f26b4253a6265f2
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-26 16:03:16 -0300

    feat: :sparkles: Functions in TypeScript

---
+++ exercises.ts
@@ -0,0 +1,25 @@
+// ./exercises.ts
+
+export function greeter(name: string) {
+  return `Olá ${name}!`;
+}
+
+export function personAge(name: string, age: number) {
+  return `${name} tem ${age} anos!`;
+}
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+  return numbers.reduce(add, 0);
+}
+
+export function triangle(base: number, height: number): number {
+  return (base * height) / 2;
+}
+
+export function square(side: number): number {
+  return side ** 2;
+}
