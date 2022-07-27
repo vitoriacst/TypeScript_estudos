24907ea36540f23ea34053f57d073cade16014e7
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-27 17:02:09 -0300

    feat: :sparkles: Type union and array

--- TS.day.02/index.ts
+++ TS.day.02/index.ts
@@ -7,9 +7,28 @@ let obj:Object = {}
 let object:{
   name:string,
   idade:number,
+  habilidades:{
+    culinárias:true,
+  }
 } = {
   name:'vivi',
-  idade:19
+  idade:19,
+  habilidades:{
+    culinárias:true
+  }
 };
 
 // sendo assim uma forma mais segura
+
+// type union -> podemo usar dois tipos para um valor
+
+let anoNascimento : number | string
+
+anoNascimento = 2003
+
+
+// array
+
+const names: (string|number)[] = ['teste']
+names.push('Vivi')
+names.push(10)
