490e873e48a10d40190cb62cd8f4a1b3891bccce
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-28 17:15:47 -0300

    feat: :zap: Create connetion and dotenv

--- Restaurant/models/connection.ts
+++ Restaurant/models/connection.ts
@@ -1,1 +1,13 @@
+import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
+dotenv.config()
+
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  Port: Number(process.env.DB_PORT)
+});
+
+export default connection;
