9158957776064e8556dade88888cc8cfe101eea8
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-28 15:35:11 -0300

    feat: :sparkles: adding error middleware

--- Express/index.ts
+++ Express/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+// ./index.ts
+import express, { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const app = express();
@@ -7,10 +8,32 @@ app.use(express.json());
 
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-  res.status(StatusCodes.OK).send('Express + TypeScript')
+app.get('/', (req: Request, res: Response) => {
+    res.status(StatusCodes.OK).send('Express + TypeScript')
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+
+  next();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
