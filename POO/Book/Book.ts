3d9008fa45d0b7db9d4e6d8298d79938b7dcd10b
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-02 17:19:17 -0300

    feat: :construction: implementing this in class

--- POO/Book/Book.ts
+++ POO/Book/Book.ts
@@ -1,9 +1,14 @@
 class Book{
   id: number;
   title: string;
-  constructor(id:number, title: string){
+  salePrice: number;
+  cost: number;
+
+  constructor(id:number, title: string,salePrice: number , cost: number){
     this.id = id;
-    this.title = title
+    this.title = title;
+    this.salePrice = salePrice;
+    this.cost = cost;
   }
 }
 
