fd73233ed12b56ab8900eccee50b68f509fc7cae
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-02 17:44:06 -0300

    feat: :sparkles: turning attributes from public to private

--- POO/Book/Book.ts
+++ POO/Book/Book.ts
@@ -1,15 +1,21 @@
 class Book{
-  id: number;
-  title: string;
-  salePrice: number;
-  cost: number;
+  private _id: number;
+  private _title: string;
+  private _salePrice: number;
+  private _cost: number;
 
   constructor(id:number, title: string,salePrice: number , cost: number){
-    this.id = id;
-    this.title = title;
-    this.salePrice = salePrice;
-    this.cost = cost;
+    this._id = id;
+    this._title = title;
+    this._salePrice = salePrice;
+    this._cost = cost;
+  }
+  // getters
+  get id(): number {
+    return this._id;
   }
 }
+
+const book = new Book(1,'Senhor dos aneis' , 90, 1)
 
 export default Book
