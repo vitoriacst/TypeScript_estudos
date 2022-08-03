1f2f75cae57af77f8c6a82bb4e7c09781a7013ff
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-03 15:45:31 -0300

    feat:

--- TypeScript/POO/heritage/person.ts
+++ TypeScript/POO/heritage/person.ts
@@ -2,3 +2,32 @@ interface Person{
   name: string;
   info: string;
 }
+
+class Student implements Person{
+  constructor(
+    public name: string,
+    public school: string,
+  ){}
+  get info(){
+    return `${this.name} estuda no ${this.school}`
+  }
+}
+
+class Professor implements Person{
+  constructor(
+    public name: string,
+    public school: string,
+    public subject: string,
+  ){}
+  get info(){
+    return `${this.name} leciona  ${this.subject} no colegio ${this.school}`
+  }
+}
+
+function printInfo(person:Person){
+  console.log(person.info);
+}
+
+const student = new Student('joao' , 'batista ')
+const professor = new Professor('joao' , 'historia' , 'bastita')
+
