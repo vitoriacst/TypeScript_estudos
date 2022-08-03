93f2c756a4f5f9fbd6f9e17c5b4f86a3a64c72b0
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-03 15:25:07 -0300

    feat: :sparkles: creating classes with interfaces

---
+++ TypeScript/POO/heritage/Animal.ts
@@ -0,0 +1,35 @@
+interface Animal {
+  name: string;
+  age: number;
+
+  getBirthDate(): Date;
+}
+
+class Bird implements Animal {
+  constructor(
+    public name: string,
+    private birthDate: Date) {}
+
+  get age() {
+    var timeDiff = Math.abs(Date.now() - new Date(this.birthDate).getTime());
+    return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+  }
+
+  getBirthDate() { return this.birthDate; }
+
+  fly() { console.log(`${this.name} está voando!`); }
+}
+
+const parrot = new Bird(
+  'Papagaio',
+  new Date(Date.parse('Aug 16, 2015')),
+);
+
+console.log(parrot.age);
+parrot.fly();
+
+/*
+Saída (código executado em Mar/2022):
+
+Papagaio está voando!
+*/
