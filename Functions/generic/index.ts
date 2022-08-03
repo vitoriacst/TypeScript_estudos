af5308a334a04e2979e440f87b4544127d900b99
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-02 21:35:47 -0300

    feat: :sparkles: implementing generic types

--- Functions/generic/index.ts
+++ Functions/generic/index.ts
@@ -3,3 +3,13 @@ function firstElement<T>(arr: T[]): T{
 }
 // podendo  receber as mesmas coisas -> sendo generic
 
+
+function mergeObjects<U, T>(obj1:U,obj2:T){
+  return {
+    ...obj1,
+    ...obj2
+  }
+}
+// dando para eles um tipo generic
+
+const newObject =  mergeObjects({name:"vivi"},{age:19})
