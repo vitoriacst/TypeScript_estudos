87b218533338b57b5a4a1506fb1a609b21ffeb34
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-08 10:58:06 -0300

    fix: :sparkles: creating interface with optional parameters

--- object/Interface.ts
+++ object/Interface.ts
@@ -6,6 +6,14 @@ interface Product {
    isAvaliable: true
 }
 
+// na interface user o nameTeste e opcional
+interface User {
+  nameTeste?: string
+  price: number
+  isAvaliable: true
+}
+
+
 function showProduct(Product: Product){
 // passamos a interface como parametro
 }
