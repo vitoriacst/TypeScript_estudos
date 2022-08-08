c6787109d367513d6e8243c479d2fac2e32a59f7
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-08 10:52:32 -0300

    feat: :sparkles: removing the interface that was received as a parameter

---
+++ object/Interface.ts
@@ -0,0 +1,11 @@
+// tipo de objeto para func com interface
+
+interface Product {
+   name: string
+   price: number
+   isAvaliable: true
+}
+
+function showProduct(Product: Product){
+// passamos a interface como parametro
+}
