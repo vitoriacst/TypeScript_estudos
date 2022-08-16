9eb315637dbdf56f611fd3c59528553eb53337b1
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-16 17:30:20 -0300

    feat: :sparkles: working with readOnly

--- class/classes.ts
+++ class/classes.ts
@@ -14,7 +14,7 @@ class NewUser {
   name
   age
   // o constructor baliza os atributos da classe , fazendo com que a tenham regras na criacao de uma nova instancia
-  
+
   constructor(name: string ,age : number){
     this.name = name;
     this.age = age;
@@ -23,3 +23,14 @@ class NewUser {
 
 const vivi = new NewUser('vivi' , 19)
 // podemos iniciar assim por conta do constructor
+
+// campo readOnly
+
+class CarTest {
+  name
+  readonly wheels = 4
+  // -|> adicionando o readOnly no atributo wheels 
+  constructor(name: string){
+    this.name = name
+  }
+}
