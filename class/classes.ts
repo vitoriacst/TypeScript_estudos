be33a6eb76551ca06caf4d037a2c1161a34132d1
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-16 17:12:47 -0300

    feat: :sparkles: creating a user class for example with constructor

--- class/classes.ts
+++ class/classes.ts
@@ -6,3 +6,20 @@ class User {
 }
 
 const vitoria = new User()
+
+
+// com o constructor
+
+class NewUser {
+  name
+  age
+  // o constructor baliza os atributos da classe , fazendo com que a tenham regras na criacao de uma nova instancia
+  
+  constructor(name: string ,age : number){
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const vivi = new NewUser('vivi' , 19)
+// podemos iniciar assim por conta do constructor
