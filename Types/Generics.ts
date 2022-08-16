3c554b11d87e3df2ba18a47f63c54362c18b6c09
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-16 11:07:10 -0300

    feat: :sparkles: generics in type Parameters

--- Types/Generics.ts
+++ Types/Generics.ts
@@ -29,3 +29,19 @@
  type Carro = MyObject<number , number>
 
  // definindo tipos genericos para os atributos da interface
+
+ // type Parameters
+
+ function getSomeKey<T, K extends keyof T>(obj: T , key: K){
+  return `A chave ${key} esta presente no objeto e tem o valor de ${obj[key]}`
+ }
+
+ const server = {
+  hd: '2TB',
+  ram: '32GB'
+ }
+
+ console.log(getSomeKey(server, 'ram'));
+ // aqui ele vai gerar o error 
+ console.log(getSomeKey(server, 'teste'));
+
